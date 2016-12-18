38396ffb412d701720da44f22584b418f0b534c1
Fonzo <aggadur@gmail.com>
2016-12-18 21:52:00 +0100

    verweis auf Brownson

--- Astronomy/module.php
+++ Astronomy/module.php
@@ -2,6 +2,7 @@
 // Modul Astronomie
 // Formeln aus "Practical Astronomy" von Peter Duffett-Smith und Jonathan Zwart, Fourth Edition
 // basiert auf den Skripten von ChokZul https://www.symcon.de/forum/threads/31467-Astronomische-Berechnungen?highlight=astronomie 
+// Twilight Grafiken generiert mit Skripten von Brownson aus der IPSLibrary
 
 class Astronomy extends IPSModule
 {
